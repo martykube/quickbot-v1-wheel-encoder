742e37a8a48920de3087481ae7a58d6ca3391813
Marty Kube <marty_kube@cable.comcast.com>
2015-06-04 11:36:57 -0400

    Offline kalaman working.

--- src/tickEncoder.m
+++ src/tickEncoder.m
@@ -11,11 +11,11 @@
 
 #rev per power
 power = 35;
-b = 0.90;
+b = 0.37;
 B = [0; b];
 
 # process noise covarriance
-Q = [100, 0.0; 0.0, 100];
+Q = [1, 0.0; 0.0, 1];
 
 # tick per rev
 N = 16; 
@@ -23,7 +23,7 @@ n = N / (2 * pi());
 C = [n, 0; 0, n];
 
 # sensor noise covarriance
-R = [1, 0; 0, 1];
+R = [5, 0; 0, 5];
 
 # Initial Conditions
 u = [power];
@@ -34,13 +34,13 @@ t_end = 50;
 # Kalman variables
 #
 x_hat = x;
-P = [2, 2; 2, 2];
+P = [1, 1; 1, 1];
 
 #
 # plot support. At each new observation...
 # 1 - time, 
-# 2,3 - sensor reading - tics and tics velocity
-# 4,5 - model prediction - tics and tics velocity
+# 2,3 - model prediction - tics and tics velocity
+# 4,5 - sensor reading - tics and tics velocity
 # 6,7 - kalman filter - tics and tics velocity
 State = [0; 0; 0; 0; 0; 0; 0];
 
@@ -63,9 +63,10 @@ for t_new = ticsData'
   tics = tics + 1;
   v_tics = 1 / dt;
   z = [tics; v_tics];
-  
-  state_i(2) = z(1);
-  state_i(3) = z(2);
+
+  # translate to state variable for comparision
+  state_i(4) = z(1) / n;
+  state_i(5) = z(2) / n;
 
   #
   # Kalman
@@ -75,8 +76,8 @@ for t_new = ticsData'
   x_hat = A * x_hat + B * u;
   P = A * P * (A') + Q;
 
-  state_i(4) = x_hat(1);
-  state_i(5) = x_hat(2);
+  state_i(2) = x_hat(1);
+  state_i(3) = x_hat(2);
 
   # update
   y_hat = z - C * x_hat;
@@ -98,8 +99,12 @@ endfor
 
 #plot(State(1, :), State(3, :), "k", State(1, :), State(5, :) , "b", State(1, :), State(7, :), "r");
 
-sensor_v_color = "r";
 model_v_color = "b";
+sensor_v_color = "r";
 kalman_v_color = 'k';
-plot(State(1, :), State(3, :), sensor_v_color, State(1, :), State(5, :), model_v_color, State(1, :), State(7, :), kalman_v_color);
-axis([0, 20, 0, 60]);
+
+#plot(State(1, :), State(2, :), model_v_color, State(1, :), State(4, :), sensor_v_color, State(1, :), State(6, :), kalman_v_color);
+
+plot(State(1, :), State(3, :), model_v_color, State(1, :), State(5, :), sensor_v_color, State(1, :), State(7, :), kalman_v_color);
+
+axis([0, 20, 0, 30]);
