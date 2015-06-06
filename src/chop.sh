6425d74fc0e79928011d928514160ab625c8c16b
Demo User <marty@beavercreekconsulting.com>
2015-06-06 20:12:36 +0000

    10s constant power test

--- src/chop.sh
+++ src/chop.sh
@@ -1,5 +1,5 @@
-scp debian@beaglebone:~/quickbot-v1-wheel-encoder/src/schmitt-1.txt ./.
-grep '^0' schmitt-1.txt | cut -d ' ' -f2,3 > schmitt-right-actual.txt
-grep '^0' schmitt-1.txt | cut -d ' ' -f2,4 > schmitt-right-filtered.txt
-grep '^1' schmitt-1.txt | cut -d ' ' -f2,3 > schmitt-left-actual.txt
-grep '^1' schmitt-1.txt | cut -d ' ' -f2,4 > schmitt-left-filtered.txt
+scp debian@beaglebone:~/quickbot-v1-wheel-encoder/src/10s-raw.txt ./.
+grep '^0' 10s-raw.txt | cut -d ' ' -f2,3 > 10s-right-actual.txt
+grep '^0' 10s-raw.txt | cut -d ' ' -f2,4 > 10s-right-filtered.txt
+grep '^1' 10s-raw.txt | cut -d ' ' -f2,3 > 10s-left-actual.txt
+grep '^1' 10s-raw.txt | cut -d ' ' -f2,4 > 10s-left-filtered.txt
